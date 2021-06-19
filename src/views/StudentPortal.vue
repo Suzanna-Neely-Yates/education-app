69e2c60ef895a8fe702dd4879ae64b0ff87dbe34
Suzanna Neely Yates <nyates@students.pitzer.edu>
2021-06-19 13:57:12 -0400

    Added elements on student portal

--- src/views/StudentPortal.vue
+++ src/views/StudentPortal.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="studentportal">
     <h1>This the student portal page</h1>
+    <v-form>
+      <v-container>
+        <!--Text field -->
+        <v-text-field label="Answer" filled outlined></v-text-field>
+      </v-container>
+    </v-form>
   </div>
 </template>
