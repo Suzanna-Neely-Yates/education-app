6febc1d66e0c5b6cebbb342d961b03e43d1593f9
Sophia Gigliotti <sgigliotti@outsideopen.com>
2021-06-19 21:21:32 -0700

    Add results

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,8 +1,36 @@
 <template>
   <div class="teacherportal">
-    <h1 class="display-2 font-weight-bold mb-3">Your CurrentStudents</h1>
+    <h1 align="center" justify="center" class="display-2 font-weight-bold ma-8">
+      Your Current Students
+    </h1>
+
     <div class="ma-5" v-for="user in users" :key="user">
-      {{ user }}
+      <v-row align="center" justify="center">
+        <v-card width="50vh" outlined>
+          <span>
+            <v-row class="ma-3">
+              <v-col cols="12" sm="8">{{ user.full_name }}</v-col>
+              <v-col v-if="user.score < -5" cols="12" class="red--text" sm="4"
+                >red zone</v-col
+              >
+              <v-col
+                v-if="user.score >= -5 && user.score < -2"
+                cols="12"
+                sm="4"
+                class="purple--text"
+                >purple zone</v-col
+              >
+              <v-col
+                v-if="user.score >= -2"
+                cols="12"
+                sm="4"
+                class="green--text"
+                >green zone</v-col
+              ></v-row
+            >
+          </span>
+        </v-card>
+      </v-row>
     </div>
   </div>
 </template>
@@ -12,7 +40,7 @@ import { db } from "../firebase";
 
 export default {
   name: "Results",
-  data: () => ({ users: [] }),
+  data: () => ({ users: [], latestSurvey: "", scores: [] }),
   components: {},
   created() {
     this.getUsers();
@@ -28,8 +56,29 @@ export default {
           querySnapshot.forEach((docu) => {
             let user = docu.data();
             user.id = docu.id;
+
+            this.getDailyScore(docu.id);
+            console.log("foo ", this.scores.get);
+            user.score = Math.floor(Math.random() * -10);
             this.users.push(user);
           });
+        });
+    },
+
+    getDailyScore(userId) {
+      console.log("score", userId);
+      db.collection("responses")
+        .where("uid", "==", userId)
+        // .where("date_submitted", ">=", new Date())
+        .get()
+        .then((querySnapshot) => {
+          let score = 0;
+          querySnapshot.forEach((docu) => {
+            let value = parseInt(docu.data().total_value);
+            console.log("valie", score);
+            if (!isNaN(value)) score += value;
+          });
+          this.scores.push(score);
         });
     },
   },
